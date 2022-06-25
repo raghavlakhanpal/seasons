21b3917a676bcf30de1007cefa9a3535c5954d46
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2022-06-25 20:04:26 +0530

    Changed to Class based component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-
-  return <div>Location :</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    return <div>Latitude:</div>;
+  }
+}
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 

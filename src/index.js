0a083d258c3fb24be3a64950dcdd2324948e5665
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2022-10-02 22:47:10 +0530

    02/10/2022

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log(position);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-    return <div>Latitude:</div>;
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errMessage: err.message });
+      }
+    );
+  }
+  render() {
+    //Conditional rendering
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error:{this.state.errMessage}</div>;
+    }
+    if (!this.state.errMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 

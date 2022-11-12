aea349f0d22a07102b2d1a5173d34599bfe0c4be
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2022-11-13 04:46:53 +0530

    Completed the project

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,41 @@
+import './SeasonDisplay.css';
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>SEASON DISPLAY!!</div>;
+//creating config objects to get rid of iterations
+const seasonConfig = {
+  winter: {
+    text: "Burr its chilly!",
+    iconName: "snowflake",
+  },
+  summer: {
+    text: "Lets hit the Beach!",
+    iconName: "sun",
+  },
+};
+
+//function to calculate the season based on month and user location
+function getSeason(lat, month) {
+  //in date() function months count starts from 0
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth);
+
+  // checking what season it is and rendering it in the return statement
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1> {text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // Initializing State - Method 1
 
-    this.state = { lat: null, errMessage: "" };
+  // constructor(props) {
+  // super(props);
+  // this.state = { lat: null, errMessage: "" };}
 
+  // Initializing State - Method 2
+  state = { lattitude: null, errMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errMessage: err.message });
-      }
+      (position) => this.setState({ lattitude: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
     );
   }
-  render() {
-    //Conditional rendering
-    if (this.state.errMessage && !this.state.lat) {
+
+  renderContent() {
+    //Conditional rendering done in helper function instead of render function
+    if (this.state.errMessage && !this.state.lattitude) {
       return <div>Error:{this.state.errMessage}</div>;
     }
-    if (!this.state.errMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+    if (!this.state.errMessage && this.state.lattitude) {
+      return <SeasonDisplay lat={this.state.lattitude} />;
     }
-    return <div>Loading...</div>;
+    return <Spinner message="Please allow location access" />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
